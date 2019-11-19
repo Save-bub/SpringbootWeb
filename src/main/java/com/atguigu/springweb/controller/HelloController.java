d009818773b458b675c85220c765886e665ab37a
Save-bub <858893128@qq.com>
2019-11-19 21:35:16 +0800

    thymeleaf

--- src/main/java/com/atguigu/springweb/controller/HelloController.java
+++ src/main/java/com/atguigu/springweb/controller/HelloController.java
@@ -2,13 +2,26 @@ package com.atguigu.springweb.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import java.util.Map;
+
+@Controller
 public class HelloController {
 
-    @GetMapping("/hello")
+    @ResponseBody
+    @RequestMapping("/hello")
     public String hello(){
         return "Hello World!";
+    }
+
+
+    //此处不能加@ResponseBody    会返回success字符串
+    @RequestMapping("/success")
+    public String success(Map<String ,Object> map){
+        map.put("hello","你好");
+        return "success";
     }
 }
