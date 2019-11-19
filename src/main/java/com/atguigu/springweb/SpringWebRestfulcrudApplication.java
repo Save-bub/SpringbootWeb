f1f2fe6e28874c73f8e7c1f3c3bcc745de7682f0
Save-bub <858893128@qq.com>
2019-11-19 20:09:48 +0800

    初始化

---
+++ src/main/java/com/atguigu/springweb/SpringWebRestfulcrudApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.springweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebRestfulcrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebRestfulcrudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/atguigu/springweb/SpringWebRestfulcrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.springweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebRestfulcrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
